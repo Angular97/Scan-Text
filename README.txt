7a4356a4c6b8fefd228bf258355b829d90115646
Angular97 <arshugupta62@gmail.com>
2021-12-15 15:36:13 +0530

    Scan Text

---
+++ README.txt
@@ -0,0 +1,34 @@
+# Scan-Text
+Scan Text App - A seamless, high-performing &amp; accessible library for OCR-related tasks .An Application for extracting text from scanned images of printed text documents.
+
+## Features
+- Crop
+- Rotate
+- Recognize Text
+- Copy to clipboard
+  
+## Libraries
+- [android-image-cropper](https://github.com/ArthurHub/Android-Image-Cropper): for image cropping.
+- [Mobile Vision](https://developers.google.com/ml-kit/vision/text-recognition/android) : To Capture and Extract the Text
+
+## Support
+- Android 6.0+
+
+## Planned Features
+
+- **Backlog**     → a simple idea 
+
+- **Ready**       → idea now is a possible feature waiting to be developed
+
+- **In progress** → feature being developed
+
+## Download Now 
+
+-[Scan Text](https://mega.nz/file/SvIgRBgD#rnzfZmZNdxm49qAyQMZfcG6OqY_M6-nUIXXpfwpJziU)
+
+## ScreenShots
+
+
+
+
+
---
+++ app/src/main/java/com/example/scantext/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.scantext;
+
+import static com.example.scantext.R.string.Retake;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button_capture,button_copy;
+    TextView textview_data;
+    Bitmap bitmap;   // it is basically  used for to store the image
+
+    private static final int REQUEST_CAMERA_CODE=100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_capture=findViewById(R.id.Button_Capture);
+        button_copy=findViewById(R.id.Button_CopyText);
+        textview_data=findViewById(R.id.text_data);
+
+        // to access the camera
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.CAMERA
+            },REQUEST_CAMERA_CODE);
+        }
+        // Now Start the Capture Button Code
+
+        button_capture.setOnClickListener
+                (view -> CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(MainActivity.this));
+
+        button_copy.setOnClickListener(view -> {
+            String scanned_data=textview_data.getText().toString();
+            copyToClipboard(scanned_data);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            CropImage.ActivityResult result=CropImage.getActivityResult(data);
+            if(resultCode==RESULT_OK){
+                Uri result_uri=result.getUri();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),result_uri);
+                    getTextFromImage(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void getTextFromImage(Bitmap bitmap)
+    {
+        TextRecognizer recognizer= new TextRecognizer.Builder(this).build();
+        if(!recognizer.isOperational()){
+            Toast.makeText(MainActivity.this,"Error Occurred!!",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Frame frame=new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> textBlockSparseArray=recognizer.detect(frame);
+            StringBuilder stringBuilder=new StringBuilder();
+            for(int i=0;i<textBlockSparseArray.size();i++)
+            {
+                TextBlock textBlock=textBlockSparseArray.valueAt(i);
+                stringBuilder.append(textBlock.getValue());
+                stringBuilder.append("\n");
+            }
+            textview_data.setText(stringBuilder.toString());
+            button_capture.setText(Retake);
+            button_copy.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void copyToClipboard(String text){
+        ClipboardManager clipboard=(ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip=ClipData.newPlainText("Copied Data",text);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(MainActivity.this,"Text Copied",Toast.LENGTH_SHORT).show();
+    }
+}
